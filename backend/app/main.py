ee72359693caca29b5e39f423afd136faa43c27e
Zaid Abdulameer <zabdulam33r@gmail.com>
2026-05-02 12:36:59 -0400

    Add backend URL to allowed CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app = FastAPI(title="Bus Tracker API")
 cors_origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://bus-tracker-api-nu.vercel.app/"
 ]
 
 app.add_middleware(
